7857fa8193512be4fdd1ca45e45ea46839aa68f8
YM <ymacedo05@gmail.com>
2022-06-04 17:29:44 -0300

    ...

--- Projetos/Mata_Mosquito/jogo.js
+++ Projetos/Mata_Mosquito/jogo.js
@@ -7,28 +7,71 @@ function ajustaTamanhoPalcoJogo(){
      altura = window.innerHeight // informa a altura da pagina
      largura = window.innerWidth // informa a largura da pagina
 
-    console.log('Largura: ' + largura + ', Altura: '+  altura)
 
 }
 
 ajustaTamanhoPalcoJogo()
-
-var posicaoX = Math.round(Math.random() * largura) - 90
-var posicaoY = Math.round(Math.random() * altura) - 90
-
-posicaoX = posicaoX < 0? 0 : posicaoX
-posicaoY = posicaoY < 0? 0 : posicaoY
-
-console.log('Left (largura): ' + posicaoX + ', Top: (altura)'+ posicaoY)
+posicaoRandomica()
 
 
-//criar o elemento html e atribuir as posições aleaórias ao elemento
+function posicaoRandomica() {
 
-var mosquito = document.createElement('img') //cria uma tag html
-mosquito.src = "imagens/mosca.png" // atruimos um atrubuto
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+    if(document.getElementById('mosq')) {
+        document.getElementById('mosq').remove()
+    }
 
-document.body.appendChild(mosquito)
+    var posicaoX = Math.round(Math.random() * largura) - 90
+    var posicaoY = Math.round(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0? 0 : posicaoX
+    posicaoY = posicaoY < 0? 0 : posicaoY
+
+    console.log('Left (largura): ' + posicaoX + ', Top: (altura)'+ posicaoY)
+
+
+    //criar o elemento html e atribuir as posições aleaórias ao elemento
+
+    var mosquito = document.createElement('img') //cria uma tag html
+    mosquito.src = "imagens/mosca.png" // atruimos um atrubuto
+    mosquito.id = 'mosq'
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+//alterando o tamanho > criamos um gerador de classes que atruí as mesmas direto no elemento criado (img)
+
+function tamanhoAleatorio(){
+
+    var classe = Math.floor(Math.random()*3)
+
+    switch (classe){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+        
+        case 2:
+            return 'mosquito3'
+    }
+    
+}
+
+
+function ladoAleatorio(){
+
+    var classe = Math.floor(Math.random()*2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        
+        case 1:
+            return 'ladoB'
+    }
+}
+
