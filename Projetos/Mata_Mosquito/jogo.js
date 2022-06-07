2ad7599922208364c726910e73b438314d670082
YM <ymacedo05@gmail.com>
2022-06-07 17:32:41 -0300

    ...

--- Projetos/Mata_Mosquito/jogo.js
+++ Projetos/Mata_Mosquito/jogo.js
@@ -1,6 +1,30 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15 
+
+var nivel  = window.location.search // search retorna as informações contidas após o sinal ?, incluindo ?.
+
+nivel = nivel.replace('?', '') // replace > substitui informações. 
+
+var atribuiVelocidade = 1500
+
+if ( nivel == 'normal') {
+    // 1500
+    atribuiVelocidade = 1500
+
+} else if (nivel == 'dificil') {
+    // 1000
+    atribuiVelocidade = 900
+
+} else if (nivel == 'radianca') {
+    // 750
+    atribuiVelocidade = 500
+}
+
+
+
 
 function ajustaTamanhoPalcoJogo(){ 
 
@@ -11,13 +35,44 @@ function ajustaTamanhoPalcoJogo(){
 }
 
 ajustaTamanhoPalcoJogo()
-posicaoRandomica()
+
+// cria um cronometro e atrui o seu valor a tag span no HTML e finaliza a execução das funções.
+
+document.getElementById('cronometro').innerHTML = tempo
+var cronometro = setInterval( function (){
+
+    tempo--
+    if (tempo < 0){
+
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = "vitoria.html"
+
+    } else{
+
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+
+}, 1000)
+
 
 
 function posicaoRandomica() {
 
     if(document.getElementById('mosq')) {
         document.getElementById('mosq').remove()
+
+        
+
+// remoção das vidas e finalização do jogo
+        if (vidas > 3){
+            window.location.href = "fim_de_jogo.html"
+        } else{
+            document.getElementById('im' + vidas).src= "imagens/coracao_vazio.png"
+
+            vidas++
+        }
     }
 
     var posicaoX = Math.round(Math.random() * largura) - 90
@@ -38,9 +93,18 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    
+    // remoção ao clicar 
+    mosquito.onclick = function() {
+       this.remove()
+    }
+
+
 
     document.body.appendChild(mosquito)
 }
+
+posicaoRandomica()
 
 //alterando o tamanho > criamos um gerador de classes que atruí as mesmas direto no elemento criado (img)
 
